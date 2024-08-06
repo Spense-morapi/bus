f20d6bcf3be50648d4f843b45104abf65402df3a
Spense-morapi <80763163+Spense-morapi@users.noreply.github.com>
2024-08-06 17:05:53 +0300

    make changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Sample;
 
 Route::view('/', 'welcome');
 
@@ -12,4 +13,5 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::get('/sample',Sample::class)->name('sample.index');
 require __DIR__.'/auth.php';
