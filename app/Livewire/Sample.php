2a9c9e8ba99c6fec05a4359fc8b76055884afecf
Spense-morapi <80763163+Spense-morapi@users.noreply.github.com>
2024-08-07 16:06:12 +0300

    Update Sample.php

--- app/Livewire/Sample.php
+++ app/Livewire/Sample.php
@@ -3,9 +3,12 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Sample extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.sample');
