f8f472505654fb28221a92dff6595b75a536808b
Spense-morapi <80763163+Spense-morapi@users.noreply.github.com>
2024-10-07 12:16:05 +0300

    okay

--- app/Livewire/Business/Business.php
+++ app/Livewire/Business/Business.php
@@ -4,13 +4,14 @@ namespace App\Livewire\Business;
 
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use App\Models\Business as BusinessModel;
 
 class Business extends Component
 {
     public function render()
     {
         $business_id = Auth::user()->business_id;
-        $businesses = Business::where('id',$business_id)->first();
+        $businesses = BusinessModel::find($business_id);
         return view('livewire.business.business',compact('businesses'));
     }
 }
